11cdf410667a4e2d509cbd8658c95327b5b9c4e4
Guilherme Souza Alves <souzaalves333@gmail.com>
2022-07-05 01:14:52 -0300

    Criação e finalização da sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import { Header } from './components/Header';
 import { Post } from './Post';
 import './global.css';
+import styles from './App.module.css';
+import { Sidebar } from './components/Sidebar';
 
 export default function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Guilherme Souza Alves"
-        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore earum nostrum necessitatibus optio consectetur sint quia officiis. Dolor rem consequatur aliquam. Dolore reprehenderit harum corrupti nulla exercitationem eaque optio perferendis."
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Guilherme Souza Alves"
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore earum nostrum necessitatibus optio consectetur sint quia officiis. Dolor rem consequatur aliquam. Dolore reprehenderit harum corrupti nulla exercitationem eaque optio perferendis."
+          />
+        </main>
+      </div>
+
     </div>
   )
 }
